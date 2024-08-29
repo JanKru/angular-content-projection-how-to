b81539aac85b734fad26de961b65e08347c7d6ef
J1nk3e <jan.kruse@live.de>
2024-08-29 19:20:56 +0200

    feat: add multiple placeholder example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeadlineComponent } from './shared/headline';
+import { CustomCardComponent } from './shared/custom-card/custom-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeadlineComponent],
-  template: '<app-headline>Content Projection</app-headline>',
-  styleUrl: './app.component.scss'
+  imports: [HeadlineComponent, CustomCardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
 }
